807916f22890a142719a0f6a1217bc5a8c8d5539
grupotucan <soportetecnico@grupotucan.com>
2025-05-20 18:53:07 +0000

    Creando el paquete de FEL GT

---
+++ l10n_gt_fel_cs/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'Guatemala FEL Invoice',
+    'version': '18.0.1.0.0',
+    'summary': 'Integrates Odoo with Guatemala FEL CS electronic invoicing service.',
+    'author': 'CSISTEMAS - Carlos Cordova',
+    'website': 'https://csistemas.com',
+    'category': 'Localization',
+    'license': 'LGPL-3',
+    'depends': ['account','l10n_gt'],
+    'data': [
+        
+        # Add your data files here, e.g. 'views/fel_cs_views.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
