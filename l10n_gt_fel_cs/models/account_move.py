e78d9c4b2fb16ffbbddfa2ccb952aa71d083085c
grupotucan <soportetecnico@grupotucan.com>
2025-05-20 22:07:56 +0000

    posted

--- l10n_gt_fel_cs/models/account_move.py
+++ l10n_gt_fel_cs/models/account_move.py
@@ -8,10 +8,17 @@ _logger = logging.getLogger(__name__)
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    def action_post(self):
-        res = super(AccountMove, self).action_post()
-        if self.move_type == 'out_invoice' and self.state=='posted':
-            for move in self:
-               # if move.partner_id.country_id.code == 'GT':
-                _logger.info("Invoice %s is from Guatemala", move.name) 
-        return res
+    def _post(self,soft=True):
+        posted = super()._post(soft=soft)
+        for move in posted:
+            _logger.info(" >>> Posted Invoice %s is from Guatemala", move.name) 
+        return posted    
+
+
+    #def action_post(self):
+    #    res = super(AccountMove, self).action_post()
+    #    if self.move_type == 'out_invoice' and self.state=='posted':
+    #        for move in self:
+    #           # if move.partner_id.country_id.code == 'GT':
+    #            _logger.info(" >>> Invoice %s is from Guatemala", move.name) 
+    #    return res
